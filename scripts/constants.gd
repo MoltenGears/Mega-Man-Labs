6af2ecc3009be341014eba1504420ce3d9eea6af
MoltenGears <47901203+MoltenGears@users.noreply.github.com>
2021-04-18 00:26:03 +0200

    Updated widescreen resolution.

--- scripts/constants.gd
+++ scripts/constants.gd
@@ -3,8 +3,8 @@ extends Node
 # Game base sizes.
 const WIDTH: int = 256
 const HEIGHT: int = 224
-const WIDTH_WIDE: int = 512
-const HEIGHT_WIDE: int = 288
+const WIDTH_WIDE: int = 384
+const HEIGHT_WIDE: int = 240
 
 # Common/Classic
 const GRAVITY: float = 15.0
@@ -17,5 +17,5 @@ const PROJECTILE_SPEED: float = 300.0  # Experimental value.
 const HIT_POINTS_MAX: int = 28
 
 # Widescreen
-const WALK_SPEED_WIDE: float = 110.0
-const PROJECTILE_SPEED_WIDE: float = 450.0  # Experimental value.
+const WALK_SPEED_WIDE: float = 110.0  # Mega Man 11 style.
+const PROJECTILE_SPEED_WIDE: float = 384.0  # Mega Man 11 style.
