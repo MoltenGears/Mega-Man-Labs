3c0040bcb14f35d2782fa476e96f3182588961f2
MoltenGears <47901203+MoltenGears@users.noreply.github.com>
2021-04-17 22:38:52 +0200

    Improved viewport leaf search.

--- general/screen.gd
+++ general/screen.gd
@@ -1,24 +1,29 @@
 extends Node2D
 
+export(String) var start_scene_release = "res://menus/TitleScreen.tscn"
+export(String) var start_scene_debug = "res://stages/heat/HeatStage.tscn"
+
 var _current_scene_path: String
 var current_scene: Node = null
 
-func _ready() -> void:
-    $"ViewportContainer/Viewport".size.x = Global.base_size.x
-    $"ViewportContainer/Viewport".size.y = Global.base_size.y
-    $"ViewportContainer".material.set_shader_param("screen_base_size", Global.base_size.y)
-    $ViewportContainer.set_process_unhandled_input(true)
+onready var _game_vp: Viewport = _find_viewport_leaf(self)
+onready var _game_vpc: ViewportContainer = _game_vp.get_parent()
 
-    if not OS.is_debug_build():
-        switch_scene("res://menus/TitleScreen.tscn")
+func _ready() -> void:
+    _game_vp.size = Global.base_size
+    _game_vpc.material.set_shader_param("screen_base_size", Global.base_size.y)
+    _game_vpc.set_process_unhandled_input(true)
+
+    if OS.is_debug_build():
+        switch_scene(start_scene_debug)
     else:
-        switch_scene("res://stages/heat/HeatStage.tscn")
+        switch_scene(start_scene_release)
 
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("action_screenshot"):
         Global.take_screenshot()
     if event.is_action_pressed("action_debug_crt"):
-        $ViewportContainer.use_parent_material = !$ViewportContainer.use_parent_material
+        _game_vpc.use_parent_material = !_game_vpc.use_parent_material
     if event.is_action_pressed("action_debug_fullscreen"):
         PixelPerfectScaling.fullscreen = !PixelPerfectScaling.fullscreen
 
@@ -35,16 +40,16 @@ func _deferred_switch_scene(path):
     _current_scene_path = path
     var new_scene: Resource = ResourceLoader.load(path)
     current_scene = new_scene.instance()
-    _get_most_inner_viewport().add_child(current_scene)
+    _game_vp.add_child(current_scene)
 
-func _get_most_inner_viewport() -> Viewport:
-    var viewport: Viewport = null
-    var child: Node = self
-    while child.get_child_count() > 0:
-        child = child.get_child(0)
-        if child is Viewport:
-            viewport = child
+func _find_viewport_leaf(node: Node) -> Viewport:
+    for child_node in node.get_children():
+        if child_node is Viewport and child_node.get_child_count() == 0:
+            return child_node
+        elif child_node.get_child_count() > 0:
+            var viewport = _find_viewport_leaf(child_node)
+            if viewport:
+                return viewport
     
-    if not viewport:
-        print("No child Viewport node found below \'", name, "\'.")
-    return viewport
+    print("No child Viewport node found below \'", name, "\'.")
+    return null
